742190e1e909cb35b458e70ff2238b293fb86822
j4pngthr <weibeicun@gmail.com>
2021-12-01 13:13:19 +0900

    クラスの第一歩

---
+++ Hito.java
@@ -0,0 +1,16 @@
+public class Hito {
+  public static void main(String[] args) {
+    Person p1 = new Person();
+
+    p1.name = "name";
+    p1.age = 20;
+
+    System.out.println("p1.name = " + p1.name);
+    System.out.println("p1.age = " + p1.age);
+  }
+}
+
+class Person {
+  String name;
+  int age;
+}
