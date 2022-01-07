f7070b0f5253109daec4d1e4d4d5113bedcfea93
j4pngthr <weibeicun@gmail.com>
2022-01-07 13:31:23 +0900

    入力された半径の円を描画

---
+++ drawCircleFrame.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*; // Appletと違う
+// swingはawtじゃないのでrepaintで上書きされる
+// http://www1.megaegg.ne.jp/~yasu/ProgrammerPage/graphics/Graphics%20Swing.html
+
+public class drawCircleFrame extends JFrame implements ActionListener {
+  public static void main(String args[]) { // Appletと違う
+    drawCircleFrame frame = new drawCircleFrame();
+    frame.setBounds(0, 0, 600, 400);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // frame.setTitle("sample");
+    frame.setVisible(true);
+  }
+
+  JTextField box1;
+  int a;
+
+  drawCircleFrame() { // public void init()の代わり
+    JPanel p = new JPanel(); // Appletの代わり
+    this.add(p, BorderLayout.CENTER);
+    box1 = new JTextField(5);
+    p.add(box1);
+    box1.addActionListener(this);
+    // Container contentPane = getContentPane(); // ないと何も表示されない
+    // contentPane.add(p, BorderLayout.CENTER);
+  }
+
+  public void actionPerformed(ActionEvent ae) { // ActionListener読んだらいる
+    String str1 = box1.getText();
+    a = Integer.parseInt(str1);
+    // clear();
+    repaint();
+  }
+
+  public void clear() {
+    Graphics g = getGraphics();
+    g.setColor(getBackground());
+    g.fillRect(0, 0, 600, 400);
+    g.dispose();
+  }
+
+  public void paint(Graphics g) {
+    g.drawOval(300 - a, 200 - a, 2 * a, 2 * a);
+  }
+}
